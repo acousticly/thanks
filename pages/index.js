106f532aaadc675c1047816272f6e444cf27b6c6
Jeve Stobs <76224164+Jeve-Stobs@users.noreply.github.com>
2021-09-29 14:38:10 -0400

    chore: defer text rotation until device model can be determined

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-const ReactRotatingText = require('react-rotating-text')
 import { BrowserView, MobileView } from 'react-device-detect'
+const ReactRotatingText = require('react-rotating-text')
 
 export default function Home() {
   return (
